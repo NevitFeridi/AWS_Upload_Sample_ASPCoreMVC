767ee2a3e7458b6496bc2b3b15a8818ee8b45d72
Navid <navidfaridi@yahoo.com>
2020-04-09 20:21:30 +0300

    add uploadFileToAWS function

--- NF.Samples.AWS_S3/NF.Samples.AWS_S3.MVC_Core/Controllers/HomeController.cs
+++ NF.Samples.AWS_S3/NF.Samples.AWS_S3.MVC_Core/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.S3;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NF.Samples.AWS_S3.MVC_Core.Models;
@@ -33,5 +35,43 @@ namespace NF.Samples.AWS_S3.MVC_Core.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region Amazon
+        string AWS_accessKey = "xxxxxxx";
+        string AWS_secretKey = "xxxxxxxxxxxxxx";
+        string AWS_bucketName = "my-uswest";
+        string AWS_defaultFolder = "MyTest_Folder";
+        protected async Task<string> UploadFileToAWSAsync(IFormFile file, string subFolder = "")
+        {
+            var result = "";
+            try
+            {
+                var s3Client = new AmazonS3Client(AWS_accessKey, AWS_secretKey, Amazon.RegionEndpoint.USWest2);
+                var bucketName = AWS_bucketName;
+                var keyName = AWS_defaultFolder;
+                if (!string.IsNullOrEmpty(subFolder))
+                    keyName = keyName + "/" + subFolder.Trim();
+                keyName = keyName + "/" + file.FileName;
+
+                var fs = file.OpenReadStream();
+                var request = new Amazon.S3.Model.PutObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName,
+                    InputStream = fs,
+                    ContentType = file.ContentType,
+                    CannedACL = S3CannedACL.PublicRead
+                };
+                await s3Client.PutObjectAsync(request);
+                
+                result = string.Format("http://{0}.s3.amazonaws.com/{1}", bucketName, keyName);
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return result;
+        }
+        #endregion
     }
 }
