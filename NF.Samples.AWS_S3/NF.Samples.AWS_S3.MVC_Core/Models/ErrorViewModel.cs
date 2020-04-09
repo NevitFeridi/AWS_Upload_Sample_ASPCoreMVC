4ffb97512c2ba1d5ae682a22339b4eae0ef750ec
Navid <navidfaridi@yahoo.com>
2020-04-09 20:11:38 +0300

    add asp.net mvc core project

---
+++ NF.Samples.AWS_S3/NF.Samples.AWS_S3.MVC_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NF.Samples.AWS_S3.MVC_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
